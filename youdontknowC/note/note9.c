4974aab79b93b275ef90825f848957cb2c075c98
PPPy290351 <testforssid03@gmail.com>
2022-03-20 00:51:03 -0700

    circular linked list by list_add_tail, V1 youdontknowC

---
+++ youdontknowC/note/note9.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <list.h>
+
+
+// TODO: list_entry
+// TODO: list_for_each
+
+struct my_list_head {
+	struct my_list_head * prev;
+	struct my_list_head * next;
+};
+
+struct cmd {
+	int a;
+	struct my_list_head list;
+};
+
+void init_list_head(struct my_list_head * head);
+void my_list_add_tail(struct my_list_head * node, struct my_list_head * head);
+void my_show_list(struct my_list_head * head);
+
+int main(void)
+{
+	struct my_list_head head_node;
+	// head pointer
+	struct my_list_head * head;
+	head = &head_node;
+	// cmd structure
+	struct cmd cmd1;
+	struct cmd cmd2;
+	struct cmd cmd3;
+	// Expected : [head ptr] -> cmd1 -> cmd2 -> cmd3
+
+	init_list_head( head );
+
+	cmd1.a = 10;
+	my_list_add_tail( &cmd1.list, head );
+	my_show_list( head );
+	printf("---------------------------\n");
+	cmd2.a = 33;
+	my_list_add_tail( &cmd2.list, head );
+	my_show_list( head );
+	// cmd3.a = 22;
+	// list_add_tail(  );
+
+
+	return 0;
+}
+
+void init_list_head(struct my_list_head * head)
+{
+	/* Let prev/next pointer pointing to itself */
+	head->prev = head;
+	//     - head - 		<- head_node : TYPE(list_head)
+	//  prev      next
+	head->next = head;
+}
+
+void my_list_add_tail(struct my_list_head * node, struct my_list_head * head)
+{
+	/**
+	 * @add_tail
+	 * 
+	 * Each node will add by [left/previous] of head
+	 * it means "head->prev = new_node"
+	 */
+	struct my_list_head * prev = head->prev;
+
+	prev->next = node; // @ [head->prev]->next
+	node->next = head;
+	node->prev = prev; // @ because new_node is "[head->prev]->next"
+	head->prev = node;
+}	
+
+void my_show_list(struct my_list_head * head)
+{
+	struct my_list_head * iterator;
+	struct cmd * cmd_result;
+
+	for (iterator=head->next; iterator != head; iterator=iterator->next) {
+		cmd_result = container_of( iterator, struct cmd, list );
+		printf("[List] a : %d\n", cmd_result->a);
+	}
+}
